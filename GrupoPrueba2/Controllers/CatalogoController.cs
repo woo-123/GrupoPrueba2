0444f44597ca28cb41f43f507bb2f12f7e762ebf
Mauricio Ortega <mauricio_ortega@usmp.pe>
2021-10-25 19:23:41 -0500

    metodo add y details catalogo controller

--- GrupoPrueba2/Controllers/CatalogoController.cs
+++ GrupoPrueba2/Controllers/CatalogoController.cs
@@ -12,10 +12,11 @@ namespace GrupoPrueba2.Controllers
     public class CatalogoController:Controller
     {
         private readonly ApplicationDbContext _context;
-
+        private readonly UserManager<IdentityUser> _userManager;
         public CatalogoController(ApplicationDbContext context)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public async Task<IActionResult> Index()
@@ -70,6 +71,34 @@ namespace GrupoPrueba2.Controllers
         {
             var productos = from o in _context.DataProducts select o;
             return View(await productos.ToListAsync());
+        }
+
+         public async Task<IActionResult> Details(int id)
+        {
+            Product objProduct = await _context.DataProducts.FindAsync(id);
+            if(objProduct == null){
+                return NotFound();
+            }
+            return View(objProduct);
+        }
+
+        public async Task<IActionResult> Add (int? id){
+            var userID = _userManager.GetUserName(User);
+            if(userID == null){
+                ViewData["Message"] = "Por favor debe loguearse antes de agregar un producto";
+                List<Product> productos = new List<Product>();
+                return View("Index",productos);
+            }else{
+                var producto = await _context.DataProducts.FindAsync(id);
+                Proforma proforma = new Proforma();
+                proforma.Producto = producto;
+                proforma.Price = producto.price;
+                proforma.Quantity = 1;
+                proforma.UserID = userID;
+                _context.Add(proforma);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
         }
     }
 } 
